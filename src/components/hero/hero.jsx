80ab62575bdc528ac2ea16294e3d4ddb9e1a97e3
Rebeca Ferreira De Almeida <arikferreiradealimeida@gmail.com>
2025-03-27 09:29:16 -0300

    2

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { getImageUrl } from '../../utils'
 
 export const Hero = () => {
-  return (
+  return <section>
     <div>
-      
+        <h1>Hi, I'm Ada</h1>
+        <p>I'm a full-stack developer with 5 years of experience, using React and NodeJS. Reach out if you'd like to learn more
+
+        </p>
+        <a href="mailto:myemail@email.com">Contact Me</a>
     </div>
-  )
+    <img src={getImageUrl("hero/heroImage.png")} alt="my image" />
+
+  </section>
+  
 }
 
 
